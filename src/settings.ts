fa00486ba3705fc77c87422e3823c5b8c0f49125
Chenyu <57704488+chenyu-psy@users.noreply.github.com>
2024-05-02 11:39:31 +0200

    bug fixed

--- src/settings.ts
+++ src/settings.ts
@@ -62,5 +62,6 @@ export const varSystem = {
   nBLUR: 0, // use to count how many times participants left the browser
   MAX_BLUR: 3, // the maximum number of times participants can leave the browser
   LOOP: true, // a switch to control whether participants need to read the instruction and practice again
+  RUN_TIMER: false, // a switch to control the countdown timer
   FAILED_ATTENTION_CHECK: false,
 };
